42af7c158cd67897af4ee2b26397e52563e8fde4
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2022-01-18 15:00:27 +0900

    Nav.js는 Head.js에 흡수됨.

--- src/component/home/Head.js
+++ src/component/home/Head.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Nav from './Nav';
+import { Link } from 'react-router-dom';
+import { navItemHead } from './NavbarItems';
 import LogoColor from '../../image/gflasColor.png';
 import LogoGray from '../../image/gflasGray.png';
+import SubNav from './SubNav';
 
-const HeadWrap = styled.div`
+const HeadNavBlock = styled.div`
+  /* background-color: purple; */
   box-sizing: border-box;
-  /* background-color: pink; */
   width: 100%;
   display: flex;
   justify-content: center;
+  :hover {
+    background-color: #fff;
+  }
+`;
+
+const HeadWrap = styled.nav`
+  display: flex;
+  max-width: 1000px;
+  justify-content: center;
+  /* background-color: pink; */
   border-bottom: 1px solid #000;
+  :hover {
+    background-color: #fff;
+    /* width: 100%; */
+    /* background-color: #dfa968; */
+  }
+  :hover .imgColor {
+    display: inline-block;
+  }
+  :hover li {
+    color: #000;
+  }
   h1 {
     margin: 0;
     padding: 0;
   }
-  .Logo {
-    position: relative;
+  .logo {
     display: inline-block;
     padding: 5px 50px;
+    position: relative;
   }
   .imgColor {
     display: none;
@@ -26,38 +49,61 @@ const HeadWrap = styled.div`
     padding: 5px 50px;
     top: 0;
     left: 0;
-    /* z-index: 99; */
   }
-  :hover .imgColor {
+`;
+
+const NavWrap = styled.ul`
+  color: #fff;
+  display: flex;
+
+  margin: 0;
+  li {
+    list-style: none;
+  }
+  li > a {
+    text-decoration: none;
+    color: inherit;
+    margin: 0;
     display: inline-block;
+    padding: 25px 35px;
   }
-  :hover {
-    background-color: #fff;
-    color: #000;
+  ul:hover {
+    /* background-color: #fff; */
   }
 `;
 
 const Head = () => {
+  const [dropdown, setDropdown] = useState(false);
   return (
-    <HeadWrap>
-      <h1 className='Logo'>
-        <img src={LogoGray} alt='Logo-Gray' />
-        <img src={LogoColor} className='imgColor' alt='Logo-Color' />
-        {/* <h1>
-          <a href='/'>
+    <HeadNavBlock>
+      <HeadWrap
+        onMouseEnter={() => {
+          setDropdown(true);
+        }}
+        onMouseLeave={() => {
+          setDropdown(false);
+        }}
+      >
+        <h1 className='logo'>
+          <Link to='./'>
             <img src={LogoGray} alt='Logo-Gray' />
-          </a>
-        </h1>
-      </div>
-      <div className='Logo2'>
-        <h1>
-          <a href='/'>
+          </Link>
+          <Link to='./'>
             <img src={LogoColor} className='imgColor' alt='Logo-Color' />
-          </a>
-        </h1> */}
-      </h1>
-      <Nav />
-    </HeadWrap>
+          </Link>
+        </h1>
+        <NavWrap>
+          {navItemHead.map(item => {
+            return (
+              <li key={item.id} className={item.cName}>
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </NavWrap>
+        {dropdown && <SubNav />}
+      </HeadWrap>
+    </HeadNavBlock>
   );
 };
 
--- src/component/home/Nav.js
+++ src/component/home/Nav.js
@@ -1,160 +1,150 @@
-import React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { navItemHead } from './NavbarItems';
-import SubNav from './SubNav';
+// import React from 'react';
+// import { useState } from 'react';
+// import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+// import { navItemHead } from './NavbarItems';
+// import SubNav from './SubNav';
+// import LogoColor from '../../image/gflasColor.png';
 
-const NavWrap = styled.ul`
-  color: #fff;
-  display: flex;
-  position: relative;
-  /* background-color: #dfa968; */
-  :hover {
-    background-color: #fff;
-    color: #000;
-  }
-  margin: 0;
-  li {
-    list-style: none;
-    /* background-color: blue; */
-    /* line-height: 47px; */
-    /* height: 16vh; */
-    /* align-items: center;
-    text-align: center; */
-  }
-  li > a {
-    text-decoration: none;
-    color: inherit;
-    margin: 0;
-    display: inline-block;
-    padding: 25px;
-    /* padding: 10px 20px; */
-    /* align-items: center; */
-  }
-  ul:hover {
-    background-color: #fff;
-  }
-  /* .subLists {
-    background-color: #fff;
-    color: #000;
-  }
-  .subLists > li {
-    display: none;
-    width: 100%;
-    height: 313px;
-    background-color: #fff;
-    border-bottom: 1px solid #e5e5e5;
-    transition: 0.3s;
-  } */
-`;
+// const NavWrap = styled.ul`
+//   color: #fff;
+//   display: flex;
+//   position: relative;
+//   :hover {
+//     width: 1000px;
+//     z-index: 99;
+//     background-color: #fff;
+//     background-color: #dfa968;
+//     color: #000;
+//     position: absolute;
+//     left: 0;
+//   }
+//   :hover .imgColor {
+//     display: inline-block;
+//   }
+//   margin: 0;
+//   li {
+//     list-style: none;
+//   }
+//   li > a {
+//     text-decoration: none;
+//     color: inherit;
+//     margin: 0;
+//     display: inline-block;
+//     padding: 25px;
+//   }
+//   ul:hover {
+//     background-color: #fff;
+//   }
+// `;
 
-// const NavHoverWrap = styled.div``;
+// // const NavHoverWrap = styled.div``;
 
-const Nav = () => {
-  const [dropdown, setDropdown] = useState(false);
-  return (
-    <>
-      <NavWrap>
-        {/* <ul className='allLists'> */}
-        {navItemHead.map(item => {
-          return (
-            <li
-              key={item.id}
-              className={item.cName}
-              onMouseEnter={() => {
-                setDropdown(true);
-              }}
-              onMouseLeave={() => {
-                setDropdown(false);
-              }}
-            >
-              <Link to={item.path}>{item.title}</Link>
-              {/* {item.title} */}
-              {/* {dropdown && <SubNav />} */}
-            </li>
-          );
-        })}
+// const Nav = () => {
+//   const [dropdown, setDropdown] = useState(false);
+//   return (
+//     <>
+//       <NavWrap>
+//         <img src={LogoColor} className='imgColor' alt='Logo-Color' />
+//         {/* <ul className='allLists'> */}
+//         {navItemHead.map(item => {
+//           return (
+//             <li
+//               key={item.id}
+//               className={item.cName}
+//               onMouseEnter={() => {
+//                 setDropdown(true);
+//               }}
+//               onMouseLeave={() => {
+//                 setDropdown(false);
+//               }}
+//             >
+//               <Link to={item.path}>{item.title}</Link>
+//               {/* {item.title} */}
+//               {/* {dropdown && <SubNav />} */}
+//             </li>
+//           );
+//         })}
 
-        {/* <li>
-          <a href='.'>유전자가위 기술</a>
-          <ul className='subLists'>
-            <li>
-              <a href='/'>CRISPR Gene Edition</a>
-            </li>
-            <li>
-              <a href='/'>CRISPR PLUS</a>
-            </li>
-            <li>
-              <a href='/'>gfCas12a</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href='.'>사업분야</a>
-          <ul className='subLists'>
-            <li>
-              <a href='/'>Cancerase®</a>
-            </li>
-            <li>
-              <a href='/'>PDB Platform</a>
-            </li>
-            <li>
-              <a href='/'>Non-GMO Edition</a>
-            </li>
-            <li>
-              <a href='/'>Popeline</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href='.'>회사소개</a>
-          <ul className='subLists'>
-            <li>
-              <a href='/'>회사소개</a>
-            </li>
-            <li>
-              <a href='/'>특허현황</a>
-            </li>
-            <li>
-              <a href='/'>연혁</a>
-            </li>
-            <li>
-              <a href='/'>리더십</a>
-            </li>
-            <li>
-              <a href='/'>홍보영상</a>
-            </li>
-            <li>
-              <a href='/'>오시는 길</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href='.'>알림마당</a>
-          <ul className='subLists'>
-            <li>
-              <a href='/'>IR 공고·공시</a>
-            </li>
-            <li>
-              <a href='/'>공지 및 안내</a>
-            </li>
-            <li>
-              <a href='/'>보도자료</a>
-            </li>
-            <li>
-              <a href='/'>인재채용</a>
-            </li>
-            <li>
-              <a href='/'>사업개발</a>
-            </li>
-          </ul>
-        </li> */}
-        {/* </ul> */}
-      </NavWrap>
-      {dropdown && <SubNav />}
-    </>
-  );
-};
+//         {/* <li>
+//           <a href='.'>유전자가위 기술</a>
+//           <ul className='subLists'>
+//             <li>
+//               <a href='/'>CRISPR Gene Edition</a>
+//             </li>
+//             <li>
+//               <a href='/'>CRISPR PLUS</a>
+//             </li>
+//             <li>
+//               <a href='/'>gfCas12a</a>
+//             </li>
+//           </ul>
+//         </li>
+//         <li>
+//           <a href='.'>사업분야</a>
+//           <ul className='subLists'>
+//             <li>
+//               <a href='/'>Cancerase®</a>
+//             </li>
+//             <li>
+//               <a href='/'>PDB Platform</a>
+//             </li>
+//             <li>
+//               <a href='/'>Non-GMO Edition</a>
+//             </li>
+//             <li>
+//               <a href='/'>Popeline</a>
+//             </li>
+//           </ul>
+//         </li>
+//         <li>
+//           <a href='.'>회사소개</a>
+//           <ul className='subLists'>
+//             <li>
+//               <a href='/'>회사소개</a>
+//             </li>
+//             <li>
+//               <a href='/'>특허현황</a>
+//             </li>
+//             <li>
+//               <a href='/'>연혁</a>
+//             </li>
+//             <li>
+//               <a href='/'>리더십</a>
+//             </li>
+//             <li>
+//               <a href='/'>홍보영상</a>
+//             </li>
+//             <li>
+//               <a href='/'>오시는 길</a>
+//             </li>
+//           </ul>
+//         </li>
+//         <li>
+//           <a href='.'>알림마당</a>
+//           <ul className='subLists'>
+//             <li>
+//               <a href='/'>IR 공고·공시</a>
+//             </li>
+//             <li>
+//               <a href='/'>공지 및 안내</a>
+//             </li>
+//             <li>
+//               <a href='/'>보도자료</a>
+//             </li>
+//             <li>
+//               <a href='/'>인재채용</a>
+//             </li>
+//             <li>
+//               <a href='/'>사업개발</a>
+//             </li>
+//           </ul>
+//         </li> */}
+//         {/* </ul> */}
+//       </NavWrap>
+//       {dropdown && <SubNav />}
+//     </>
+//   );
+// };
 
-export default Nav;
+// export default Nav;
