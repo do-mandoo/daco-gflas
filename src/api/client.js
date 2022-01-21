680e60421c1919dded52e33e8382a9f7e9eebfb6
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2022-01-21 22:52:40 +0900

    서버주소변경

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: 'http://34.64.135.42:5000' });
+const client = axios.create({ baseURL: 'http://34.64.207.232:5000' });
 
 export default client;
